2c44809485599aeee34a461582cf065811562f80
cyz <1285247409@qq.com>
2022-04-03 22:32:58 +0800

    p22-使用openFeign进行远程调用服务接口

--- gulimall-member/src/main/java/com/atguigu/gulimall/gulimallmember/controller/MemberController.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/gulimallmember/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.gulimallmember.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.atguigu.gulimall.gulimallmember.feign.CouponFeignService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,19 @@ import com.atguigu.gulimall.common.utils.R;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity memberEntity =new MemberEntity();
+        memberEntity.setNickname("张三");
+        R membercoupons = couponFeignService.memebercoupons();
+        membercoupons.get("");
+
+        return  R.ok().put("member",memberEntity).put("coupons",membercoupons.get("coupons"));
+    }
 
     /**
      * 列表
